3a3969ba685e399c8e16288048237073efe7c251
HenTatl <alien2amik@gmail.com>
2022-10-16 20:58:05 +0200

    Example005_Condition_If_Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input User Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "henry")
+{
+    Console.WriteLine("Ura, You are HENRY");
+}
+else
+{
+    Console.Write("Hellou, ");
+    Console.WriteLine(username);
+}
+
